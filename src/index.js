4fd9c55d6f4d6db85607ad2423aea67523cc1ce1
annie ke <annieke8@gmail.com>
2017-04-18 10:35:49 -0400

    committing files to master

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const $ = require('jquery');
+import $ from 'jquery';
+import './style.scss';
 
-$('#main').html('Here we go!'); 
+let seconds = 0;
+
+setInterval(() => {
+  seconds += 1;
+  $('#main').html(`You've been on this page for ${seconds} seconds.`);
+}, 1000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,56 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'style.css',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 module.exports = {
-  entry: [ './src' ], // this is where our app lives
-  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  entry: ['./src'],
+  devtool: 'inline-source-map',
   output: {
-    path: path.join(__dirname, 'public/build'), ////output files go to public/build
+    path: path.join(__dirname, 'public/build'),
     publicPath: '/build/',
-    filename: 'bundle.js', //package it all up in one bundle
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'), //any static files should go in public
-    historyApiFallback: true, // useful later
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
   },
+  module: {
+    rules: [
+      {
+        test: /(\.scss)$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [autoprefixer()],
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ],
+          fallback: 'style-loader', // use style-loader in development mode
+        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  plugins: [
+    extractSass,
+  ],
 };

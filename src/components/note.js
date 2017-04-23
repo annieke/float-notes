64af1416f707dba44e5014fdd7ac2c5634d53c2a
annie ke <annieke8@gmail.com>
2017-04-23 01:55:05 -0400

    editable notes

--- src/components/note.js
+++ src/components/note.js
@@ -1,47 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Draggable from 'react-draggable';
+import Textarea from 'react-textarea-autosize';
 
-const Note = (props) => {
-  const title = props.note.title;
-  const text = props.note.text;
-  // const noteX = props.note.x;
-  // const noteY = props.note.y;
-  // DEBUG FIGURE OUT WHAT TO DO
+class Note extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Draggable
-      handle=".drag-button"
-      grid={[25, 25]}
-      defaultPosition={{ x: 20, y: 20 }}
-      position={null}
-      onStart={Draggable.handleStart}
-      onDrag={Draggable.handleDrag}
-      onStop={Draggable.handleStop}
-    >
-      <div className="note">
-        <div className="row">
-          <div>
-            <div className="note-title">{title}</div>
+    this.state = {
+      id: props.note.id,
+      title: props.note.title,
+      text: props.note.text,
+      onDelete: props.onDelete,
+      onDoneEdit: props.onDoneEdit,
+      isEditing: false,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  editToggle() {
+    if (!this.state.isEditing) {
+      return (<i className="fa fa-pencil" aria-hidden="true" />);
+    } else {
+      return (<i className="fa fa-check" aria-hidden="true" />);
+    }
+  }
+
+  renderText() {
+    if (this.state.isEditing) {
+      return (
+        <div>
+          <Textarea onChange={this.onInputChange} value={this.state.text} />
+        </div>
+      );
+    } else {
+      return (
+        <div>{this.state.text}</div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <Draggable
+        handle=".drag-button"
+        grid={[25, 25]}
+        defaultPosition={{ x: 20, y: 20 }}
+        position={null}
+        onStart={Draggable.handleStart}
+        onDrag={Draggable.handleDrag}
+        onStop={Draggable.handleStop}
+      >
+        <div className="note">
+          <div className="row">
+            <div className="title-container">
+              <div className="note-title">{this.state.title}</div>
+              <div>
+                <button onClick={() => this.state.onDelete(this.state.id)}>
+                  <i className="fa fa-trash" aria-hidden="true" />
+                </button>
+                <button onClick={() => {
+                  if (!this.state.isEditing) {
+                    this.setState({ isEditing: true });
+                  } else {
+                    this.state.isEditing = false;
+                    const IDtext = {
+                      id: this.state.id,
+                      text: this.state.text,
+                    };
+                    this.state.onDoneEdit(IDtext);
+                  }
+                }}
+                >
+                  {this.editToggle()}
+                </button>
+              </div>
+            </div>
+
             <div>
-              <button>
-                <i className="fa fa-trash" aria-hidden="true" />
-              </button>
-              <button>
-                <i className="fa fa-pencil" aria-hidden="true" />
+              <button className="drag-button">
+                <i className="fa fa-arrows-alt" aria-hidden="true" />
               </button>
             </div>
           </div>
-
-          <div>
-            <button className="drag-button">
-              <i className="fa fa-arrows-alt" aria-hidden="true" />
-            </button>
-          </div>
+          <div>{this.renderText()}</div>
         </div>
-        <div>{text}</div>
-      </div>
-    </Draggable>
-  );
-};
+      </Draggable>
+    );
+  }
+}
 
 export default Note;
--- src/components/note_board.js
+++ src/components/note_board.js
@@ -7,37 +7,20 @@ class NoteBoard extends Component {
   constructor(props) {
     super(props);
 
-    const initMap = {
-      1: {
-        id: 1,
-        title: 'hi',
-        text: 'hellooooo',
-        x: 0,
-        y: 0,
-        zIndex: 0,
-      },
-      2: {
-        id: 2,
-        title: 'banana',
-        text: 'apple',
-        x: 10,
-        y: 11,
-        zIndex: 1,
-      },
-    };
-
     this.state = {
-      notes: Immutable.Map(initMap),
-      currID: 3,
+      notes: Immutable.Map(),
+      currID: 0,
     };
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onDoneEdit = this.onDoneEdit.bind(this);
   }
 
-  onSubmit(notetitle) {
+  onSubmit(newnotetitle) {
     const newnote = {
       id: this.state.currID,
-      title: notetitle,
+      title: newnotetitle,
       text: '',
       x: 0,
       y: 0,
@@ -49,15 +32,33 @@ class NoteBoard extends Component {
     this.state.currID += 1;
   }
 
+  onDelete(id) {
+    this.setState({
+      notes: this.state.notes.delete(id),
+    });
+  }
+
+  onDoneEdit(IDtext) {
+    this.setState({
+      notes: this.state.notes.update(IDtext.id, (n) => {
+        return Object.assign({}, n, IDtext);
+      }),
+    });
+  }
+
   render() {
     const noteboard = this.state.notes.entrySeq().map(([id, note]) => {
-      return <Note key={id} note={note} />;
+      return (<Note key={id} note={note}
+        onDelete={this.onDelete} onDoneEdit={this.onDoneEdit}
+      />);
     });
 
     return (
       <div>
         <NewNoteBar onSubmit={this.onSubmit} />
-        {noteboard}
+        <div>
+          {noteboard}
+        </div>
       </div>
     );
   }
